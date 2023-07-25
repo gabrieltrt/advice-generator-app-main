6180e54df5c4f577269db4f22d777759830d84d4
Tertuh <gabriel.trtl@gmail.com>
2023-07-25 11:08:49 -0300

    v1.0

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch('https://api.adviceslip.com/advice')
+
+.then(response => response.json())
+.then(data => {
+    const singleId = document.getElementById('adviceNumber');
+    const adviceParagraph = document.getElementById('advice');
+
+   // Exibir o ID único no elemento com ID "singleId"
+    singleId.textContent = `${data.slip.id}`;
+
+    // Exibir o parágrafo no elemento com ID "adviceParagraph"
+    adviceParagraph.textContent = data.slip.advice;
+
+    console.log(data)
+})
+
+
+
+
+
+
